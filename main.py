f21cca5e268be328815f0a5c8b8b561c499a9921
illiabranchuk <jointlord0@gmail.com>
2024-08-27 18:30:27 +0100

    check_gradient

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from metrics import multiclass_accuracy
 
 
 def prepare_for_linear_classifier(train_X, test_X):
-    train_flat = train_X.reshape(train_X.shape[0], -1).astype(np.float) / 255.0
-    test_flat = test_X.reshape(test_X.shape[0], -1).astype(np.float) / 255.0
+    train_flat = train_X.reshape(train_X.shape[0], -1).astype(np.float64) / 255.0
+    test_flat = test_X.reshape(test_X.shape[0], -1).astype(np.float64) / 255.0
     
     mean_image = np.mean(train_flat, axis = 0)
     train_flat -= mean_image
@@ -24,7 +24,7 @@ train_X, train_y, val_X, val_y = random_split_train_val(train_X, train_y, num_va
 
 def main():
     train_X, train_y, test_X, test_y = load_svhn("data", max_train=1000, max_test=100)
-    prepare_for_linear_classifier(train_X,test_X)
+    print(prepare_for_linear_classifier(train_X,test_X))
     
 if __name__ == "__main__":
     main()
